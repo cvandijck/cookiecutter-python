c4725aca1333340bfb7569b7494a34c16d6ff640
Christophe Van Dijck <christophe.vdijck@gmail.com>
2024-08-08 20:12:13 +0200

    fixes

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -8,18 +8,18 @@ PYTHON_EXE = sys.executable
 
 def test_import_package():
     """Test basic import of package."""
-    import {{cookiecutter.package_name}}
+    import {{cookiecutter.package_name}}  # noqa: F401
 
 
 @pytest.mark.console
 def test_console_help():
-    """Calls help file of console script and tests for failure"""
+    """Calls help file of console script and tests for failure."""
     process = subprocess.run([PYTHON_EXE, '-m', '{{cookiecutter.package_name}}', '--help'], capture_output=True, universal_newlines=True)
     assert process.returncode == 0, process.stderr
 
 
 @pytest.mark.console
 def test_console_noargs():
-    """Runs console without arguments expecting to fail"""
+    """Runs console without arguments expecting to fail."""
     process = subprocess.run([PYTHON_EXE, '-m', '{{cookiecutter.package_name}}'], capture_output=True, universal_newlines=True)
     assert process.returncode == 2, process.stderr
