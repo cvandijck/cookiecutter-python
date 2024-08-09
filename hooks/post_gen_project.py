c9ffcca5194275daecbee04705be0b973b4619fd
Christophe Van Dijck <christophe.vdijck@gmail.com>
2024-08-09 21:32:03 +0200

    fix git script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,12 +4,15 @@ import sys
 
 LOGGER = logging.getLogger(__name__)
 
-INITIALIZE_REPO = bool({{cookiecutter.initialize_repo}})
+INITIALIZE_REPO = {{cookiecutter.initialize_repo}}
 DEFAULT_BRANCH_NAME = 'main'
 REPO_URL = '{{cookiecutter.url}}.git'
 
+LOGGER.warning(INITIALIZE_REPO)
+
 
 def _execute_command(command: list[str]):
+    LOGGER.warning(' '.join(command))
     subprocess.run(command, capture_output=True, check=True)
 
 
@@ -22,16 +25,12 @@ def is_git_installed() -> bool:
 
 
 def initialize_repo(repo_url: str, branch: str):
-    try:
-        _execute_command(['git', 'init'])
-        _execute_command(['git', 'add', '.'])
-        _execute_command(['git', 'commit', '-m', 'cookiecutter structure'])
-        _execute_command(['git', 'branch', '-M', branch])
-        _execute_command(['git', 'remote', 'add', 'origin', repo_url])
-        _execute_command(['git', 'push', '-U', 'origin', branch])
-        return True
-    except Exception:
-        return False
+    _execute_command(['git', 'init'])
+    _execute_command(['git', 'add', '.'])
+    _execute_command(['git', 'commit', '-m', 'cookiecutter structure'])
+    _execute_command(['git', 'branch', '-M', branch])
+    _execute_command(['git', 'remote', 'add', 'origin', repo_url])
+    _execute_command(['git', 'push', '-u', 'origin', branch])
 
 
 if __name__ == '__main__':
