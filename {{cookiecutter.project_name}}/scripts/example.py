afab26ddb0ad4e5d49c05cde27e4610e6819f76c
Christophe Van Dijck <christophe.vdijck@gmail.com>
2025-02-23 21:07:31 +0100

    add example script for script dir

---
+++ {{cookiecutter.project_name}}/scripts/example.py
@@ -0,0 +1,9 @@
+from {{cookiecutter.package_name}} import __version__
+
+
+def main():
+    print('Running {{cookiecutter.package_name}} version', __version__)
+
+
+if __name__ == '__main__':
+    main()
